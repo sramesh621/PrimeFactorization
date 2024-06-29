1880a8c614405bca0ed3b1c73a3229cbefbea2ae
sramesh621 <ramesh.srigiriraju@gmail.com>
2024-06-29 12:28:49 -0400

    Update primeFactorization.c

--- primeFactorization.c
+++ primeFactorization.c
@@ -4,15 +4,17 @@ int* toBinaryArray(int i)
            {
                 int bitLength = (int)log2(i) + 1;
                 int* binaryArray = (int*)malloc(4 * bitLength);
-                int* binaryArrayPtr = (int*)((int)binaryArray + 4 * (bitLength - 1));
+                int* binaryArrayPtr = (int*)binaryArray;
                 int copy = i;
                 int radix = 0;
                 while(copy >= 0)
                 {
-                    int remainder = copy & 1;
-                    *binaryArrayPtr = remainder;
-                    binaryArrayPtr = (int*)((int)binaryArrayPtr - 4);
+                   int remainder = copy & 1;
+                   *binaryArrayPtr = remainder;
+                   binaryArrayPtr = (int*)((int)binaryArrayPtr + 4);
+                   copy >>= 1;
                 }
+                return binaryArray;
             }
 
             function factorTheKey()
