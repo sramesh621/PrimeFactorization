8a35d2c504b0b84e7d3cc23f6905c5c170ca7020
sramesh621 <ramesh.srigiriraju@gmail.com>
2024-06-29 12:13:36 -0400

    Update primeFactorization.c

--- primeFactorization.c
+++ primeFactorization.c
@@ -1,1 +1,24 @@
 
+
+            int* toBinaryArray(int i)
+           {
+                int bitLength = 
+                int* binaryArray = (int*)malloc(4 * bitLength);
+                var copy = i;
+                var radix = 0;
+                while(copy >= 0)
+                {
+                
+                }
+            }
+
+            function factorTheKey()
+            {
+            var product = document.getElementById(“keyInputId”).value;
+                var maxFactor = Math.sqrt(product);
+                var i;
+                for(i = 2; i<= maxFactor; i++)
+                {
+                    var primeRepArray = toBinaryArray(i);
+                }
+            }
