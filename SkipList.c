fc887a896b98472c1b8c667fb347e0efcebb3d18
sramesh621 <ramesh.srigiriraju@gmail.com>
2024-10-22 20:01:54 -0400

    Update SkipList.c

--- SkipList.c
+++ SkipList.c
@@ -6,3 +6,33 @@ void initSkipList(SkipList* const initMe)
     initMe->index = 0;
     initMe->nextNode = NULL;
 }
+
+int getListIndex(SkipListNode* const skipList, byte numDigits)
+{
+    int toReturn = -1;
+    if(skipList != NULL)
+    {
+        SkipListNode* previousNode = NULL;
+        SkipListNode* currentNode = skipList;
+        while(currentNode != NULL)
+        {
+            if(currentNode->numDigits >= numDigits)
+            {
+                break;
+            }
+            previousNode = currentNode;
+            currentNode = currentNode->nextNode;
+        }
+    }
+    if(currentNode != NULL)
+    {
+        toReturn = currentNode->index;
+    }
+    return toReturn;
+}
+
+void addListNode(SkipListNode* const skipList, byte numDigits, int location)
+{
+    
+}
+void deleteSkipList(SkipListNode* const toDelete);
